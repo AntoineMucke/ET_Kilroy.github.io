b2d2e5aedc0bc4b94a5cc356eb6e4f6ae587e839
Antoine Mucke <antoine.mucke@orange.fr>
2024-12-05 10:01:13 +0100

    v2

--- niveau1.js
+++ niveau1.js
@@ -1,8 +1,4 @@
-{
-    "users": [
-        { "username": "admin", "password": "admin123" },
-        { "username": "user", "password": "user123" }
-    ],
-    "success": false,
-    "message": "Identifiants invalides. Essayez encore."
-}
+[
+    { "username": "admin", "password": "admin123" },
+    { "username": "user", "password": "user123" }
+]
